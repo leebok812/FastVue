2a24823b84ec116075a32358cfdaab48030636fc
JUNEBOK <leebok0812@gmail.com>
2021-05-24 22:33:45 +0900

    Vue 설치 및 최상위 컴포넌트 (App.vue)설정

--- Vue-Todo-App/App.vue
+++ Vue-Todo-App/App.vue
@@ -1,16 +1,22 @@
 <template>
-    
+    <h1>{{msg}}</h1>
 </template>
 
 
 <script>
 export default {
-
+    data () {
+        return {
+            msg: 'Hello Vue!'
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+h1{
+    color : red;
+}
 
 </style>
 
--- Vue-Todo-App/main.js
+++ Vue-Todo-App/main.js
@@ -1,1 +1,15 @@
-console.log('Hello webpack!')
+import Vue from 'vue'
+import App from './App.vue'
+
+
+new Vue({
+
+    el:'#app',
+    // render (createElement) {
+    //     return createElement(App)
+    // }, 밑에랑 같은 말임
+    render: h => h(App)
+})
+
+
+
