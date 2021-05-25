b06337b29120bca82e6d34448308a54eb3f82457
JUNEBOK <leebok0812@gmail.com>
2021-05-25 23:01:14 +0900

    Babel 설치 및 설정(.babelrc) 그리고 @babel_polyfill

--- Vue-Todo-App/webpack.config.js
+++ Vue-Todo-App/webpack.config.js
@@ -1,11 +1,16 @@
 
 const path = require('path') //nodeJs에서 제공하는 path 모듈
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+require('@babel/polyfill')
 
 
 module.exports = {
     entry: { //진입점
-        app: path.join(__dirname, 'main.js')  //app은 진입하는 파일의 별칭 || __dirname 은 nodeJS에서 제공하는 현재파일의 위치를 제공하는 전역변수
+        app: [
+            '@babel/polyfill',
+            path.join(__dirname, 'main.js') //app은 진입하는 파일의 별칭 || __dirname 은 nodeJS에서 제공하는 현재파일의 위치를 제공하는 전역변수
+        ] 
+        
     }, 
     output: { // 결과물에 대한 설정
         filename: '[name].js', // 결과물이 나오는 이름 || 대괄호 [] 안에 name을 넣어주면 진입점 app이 자동으로 박힘
